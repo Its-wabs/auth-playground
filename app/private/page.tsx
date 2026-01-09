179023e0faec6d42d12c683f548d02a921bb05cc
normydevbop <cherfi.m.abdelwahab@gmail.com>
2026-01-09 12:28:58 +0100

    refactored /private

--- app/private/page.tsx
+++ app/private/page.tsx
@@ -1,17 +1,19 @@
-
+import { redirect } from "next/navigation";
 import LogoutButton from "../components/logout";
 import { getSession } from "@/lib/auth";
 
-export default async function PrivatePage({ params }: { params?: any }) {
-
-
+export default async function PrivatePage() {
   const session = await getSession();
+
+  if (!session) {
+    redirect("/?auth=login");
+  }
 
   return (
     <div>
       <h1>Private Page</h1>
       <p>Welcome {session.user.email}</p>
-      <LogoutButton/>
+      <LogoutButton />
     </div>
   );
 }
