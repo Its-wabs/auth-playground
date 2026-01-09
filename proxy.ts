af5ec7d5627e7a203fd351229d58359320bc48ee
normydevbop <cherfi.m.abdelwahab@gmail.com>
2026-01-09 11:44:57 +0100

    fixed prisma client

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import prisma from "./lib/prisma";
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const url = req.nextUrl.clone();
 
   // Skip public routes
